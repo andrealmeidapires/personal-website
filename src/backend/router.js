9c8885bddb83246af333249e699360e0a425f030
André Pires <ruiandrepires@gmail.com>
2017-01-26 16:55:34 +0000

    former index.html js code put into scripts.js file. backend code reuse optimization

--- src/backend/router.js
+++ src/backend/router.js
@@ -3,11 +3,8 @@ function route(request, response, handle, pathname, postData, hostname){
   if (typeof handle[pathname] === "function") {
     handle[pathname](request, response, postData, hostname);
 
-  } else if(/\.(jpe?g|png|gif|bmp)$/i.test(pathname)) {
-    handle["/getImage"](request, response, postData, hostname);
-
-  } else if(/\.css$/i.test(pathname)) {
-    handle["/getCSS"](request, response, postData, hostname);
+  } else if(/\.(js|css|jpe?g|png|gif|bmp)$/i.test(pathname)) {
+    handle["/getFile"](request, response, postData, hostname);
 
   } else {
     console.error("No request handler found for", pathname);
